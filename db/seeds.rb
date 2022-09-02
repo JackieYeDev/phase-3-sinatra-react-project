48ab09c2b9c237a5e96042bb334b8e056236d538
Jackie <jye0325@gmail.com>
2022-09-01 21:22:25 -0400

    Update seeds.rb file with more meaningful seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ john = Author.create(name: "John Smith")
 peter = Author.create(name: "Peter Knight")
 
 
-Note.create(title: "", content: "", author_id: jane.id)
-Note.create(title: "", content: "", author_id: jane.id)
-Note.create(title: "", content: "", author_id: john.id)
-Note.create(title: "", content: "", author_id: john.id)
-Note.create(title: "", content: "", author_id: john.id)
-Note.create(title: "", content: "", author_id: peter.id)
+Note.create(title: "Jane's First Note", content: "The cake is a lie.", author_id: jane.id)
+Note.create(title: "Jane's Second Note", content: "I need to buy groceries to meal prep.", author_id: jane.id)
+Note.create(title: "John's First Note", content: "Password: 123456", author_id: john.id)
+Note.create(title: "John's Second Note", content: "Cranberry juice is good for cleansing your system.", author_id: john.id)
+Note.create(title: "John's Third Note", content: "My favorite food is ramen.", author_id: john.id)
+Note.create(title: "Peter's First Note", content: "Reminder: Important meeting tomorrow at 10am.", author_id: peter.id)
 
 puts "✅ Done seeding!"
