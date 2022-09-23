d9c3acab92cd4dbc032b74dae7e6ab6cf1f3b275
Jackie <jye0325@gmail.com>
2022-09-23 11:28:44 -0400

    Add serialization to fetch notes by author id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,57 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
+  def serialize(objects)
+    objects.to_json(
+      only: [:id],
+      include: {
+        notes: {
+          only: [
+            :id,
+            :title,
+            :content
+          ]
+        }
+      }
+    )
+  end
+
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
 
+  # Return authors with associated notes.
   get "/authors" do
     authors = Author.all
     authors.to_json
   end
 
+  get "/authors/:id" do
+    serialize(Author.find(params[:id]))
+  end
+
   get "/notes" do
     notes = Note.all
     notes.to_json
+  end
+
+  post "/notes" do
+    author = Author.where(name: params[:name]).first_or_create
+    note = Note.create(
+      title: params[:title],
+      content: params[:content],
+      author_id: params[:author_id]
+    )
+    note.to_json
+  end
+
+  patch "/notes" do
+
+  end
+
+  delete "/notes" do
+
   end
 
 end
